e5ecfb60561cbc36f36a125f670f1340f5e41784
neil paul molina <get.neilmolina@gmail.com>
2021-09-10 19:49:54 +0800

    fix production deployment

--- function.js
+++ function.js
@@ -6,7 +6,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 const server = next({
   dev: isDev,
   //location of .next generated after running -> yarn build
-  conf: { distDir: '.next' },
+  conf: require('./next.config'),
 });
 
 const nextjsHandle = server.getRequestHandler();
